3da0c943a8ccf32800ff77fa5dd58273e51f17fc
YA <vector_cp@mail.ru>
2022-04-23 17:25:33 +0300

    feat:  add no-scroll on burger-open. Main, pets pages behavior is done!

--- shelter/assets/js/drop-menu-service.js
+++ shelter/assets/js/drop-menu-service.js
@@ -1,23 +1,48 @@
-console.log('drop-menu-service.js');
+// console.log('drop-menu-service.js');
+
 // FUNCS
+
+//have a shake eff(
+const disableScrolling = () => {
+  document.body.style.overflowY = '-1';
+  window.addEventListener('scroll', function (e) {
+    window.scrollTo(0, 0);
+  });
+};
+
+const enableScrolling = () => {
+  window.onscroll = function () {};
+};
+
 // toggler
 const openCloseBurger = () => {
-  // console.log('#openCloseBurger');
-  /*menuToggler.checked === true
-    ? MenuDrop.classList.add('show-dropdown')
-    : MenuDrop.classList.remove('show-dropdown');
-    */
+  if (menuToggler.checked === true) {
+    MenuDrop.classList.add('show-dropdown');
+    blackout.classList.add('active');
+    document.body.classList.add('prevent-scroll');
+    // disableScrolling();
 
-    if (menuToggler.checked === true) {
-      MenuDrop.classList.add('show-dropdown');
-      blackout.classList.add('active');
-      document.body.style.overflowY = "hidden";
-    } else {
-      MenuDrop.classList.remove('show-dropdown');
-      blackout.classList.remove('active');
-      document.body.style.overflowY = "visible"
-
+    //trick for rets page fixed header)
+    if (headerPosition !== null) {
+      headerPosition.style.position = 'static';
+      main.style.marginTop = '5px';
     }
+  } else {
+    MenuDrop.classList.remove('show-dropdown');
+    blackout.classList.remove('active');
+    document.body.classList.remove('prevent-scroll');
+    // enableScrolling();
+
+    if (headerPosition !== null) {
+      headerPosition.style.position = 'fixed';
+      main.style.marginTop = '120px';
+    }
+  }
+};
+
+const closeBurger = () => {
+  menuToggler.checked = false;
+  openCloseBurger();
 };
 
 // DOM
@@ -26,21 +51,24 @@ const menuToggler = document.querySelector('#menu__toggle');
 const anchors = document.querySelectorAll(
   '.header__menu__link__anchor'
 );
-
-const blackout = document.querySelector(
-  '.pets__blackout'
-);
+const blackout = document.querySelector('.pets__blackout');
+const headerPosition =
+  document.querySelector('.header--position') || null;
+const main = document.querySelector('.main');
 
 // START
-// set after reload
+// close menu after reload
 menuToggler.checked = false;
 
 menuToggler.addEventListener('change', openCloseBurger);
+blackout.addEventListener('click', closeBurger);
 
 // go and close after link
 for (let anchor of anchors) {
   anchor.addEventListener('click', function (e) {
     e.preventDefault();
+
+    closeBurger();
 
     const blockID = anchor.getAttribute('href');
 
@@ -48,8 +76,5 @@ for (let anchor of anchors) {
       behavior: 'smooth',
       block: 'start',
     });
-
-    menuToggler.checked = false;
-    openCloseBurger();
   });
 }

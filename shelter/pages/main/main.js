ae0397aa93a7df34cbf26f39dd304791e326a68b
YA <vector_cp@mail.ru>
2022-04-24 14:02:40 +0300

    feat: main page => add carusel

--- shelter/pages/main/main.js
+++ shelter/pages/main/main.js
@@ -1,8 +1,8 @@
-console.log('main.js');
+// responsive slider. for now extremely difficult)
 
-// FUNCS
 const formPet = (pet) => {
-  const card = `<div class="pets__slider__card">
+  const card = `
+  <div class="pets__slider__card">
   <div class="pets__card__header">
       <img src="${pet.img}" alt="${pet.name}" />
   </div>
@@ -15,25 +15,140 @@ const formPet = (pet) => {
   return card;
 };
 
+// FUNCS
+
+const shuffle = (array) => {
+  return array.sort(() => Math.random() - 0.4);
+};
+
+const clearOpacity = () => {
+  blocks[2].style.opacity = 1;
+  blocks[1].style.opacity = 1;
+  blocks[0].style.opacity = 1;
+};
+
+const slidePrev = (petsArr) => {
+  let __bl = blocks.shift();
+  blocks.push(__bl);
+
+  blocks[0].style.transform = 'translate(-100%,0)';
+  blocks[1].style.transform = 'translate(0%,0)';
+  blocks[2].style.opacity = 0;
+  blocks[2].style.transform = 'translate(100%,0)';
+
+  setTimeout(() => {
+    clearOpacity();
+    add(petsArr);
+  }, 900);
+};
+
+const slideNext = () => {
+  let __bl = blocks.pop();
+  blocks.unshift(__bl);
+
+  blocks[0].style.opacity = 0;
+  blocks[0].style.transform = 'translate(-100%,0)';
+  blocks[1].style.transform = 'translate(0%,0)';
+  blocks[2].style.transform = 'translate(100%,0)';
+
+  setTimeout(() => {
+    clearOpacity();
+    add(petsArr);
+  }, 900);
+};
+
+const add = (petsArr) => {
+  let current = [];
+  for (let index = 0; index < 3; index++) {
+    const _cur = blocks[1].children[index].textContent
+      .trim()
+      .split('\n')[0];
+    current.push(_cur);
+  }
+
+  petsArr = petsArr.filter(
+    (el) =>
+      el.name != current[0] &&
+      el.name != current[1] &&
+      el.name != current[2]
+  );
+
+  let arr = [...petsArr];
+  arr = shuffle(arr);
+
+  blocks[0].innerHTML = '';
+  arr.slice(0, 3).forEach((pet) => {
+    blocks[0].insertAdjacentHTML('beforeend', formPet(pet));
+  });
+
+  blocks[2].innerHTML = '';
+  arr.slice(2, 5).forEach((pet) => {
+    blocks[2].insertAdjacentHTML('beforeend', formPet(pet));
+  });
+};
+
+const set = (petsArr) => {
+  petsArr = shuffle(petsArr);
+
+  petsArr.slice(3, 6).forEach((pet) => {
+    slidesBox1.insertAdjacentHTML(
+      'beforeend',
+      formPet(pet)
+    );
+  });
+
+  petsArr.slice(0, 3).forEach((pet) => {
+    slidesBox2.insertAdjacentHTML(
+      'beforeend',
+      formPet(pet)
+    );
+  });
+
+  petsArr.slice(5, 8).forEach((pet) => {
+    slidesBox3.insertAdjacentHTML(
+      'beforeend',
+      formPet(pet)
+    );
+  });
+};
+
 // DOM
+
 const cardsContainer = document.querySelector(
-  '.pets__slider__cards-1'
+  '.pets__slider__cards'
 );
 
+const slidesBox1 = document.createElement('div');
+slidesBox1.className = 'slider_way';
+slidesBox1.style.transform = 'translate(-100%,0)';
+cardsContainer.append(slidesBox1);
+
+const slidesBox2 = document.createElement('div');
+slidesBox2.className = 'slider_way';
+cardsContainer.append(slidesBox2);
+
+const slidesBox3 = document.createElement('div');
+slidesBox3.className = 'slider_way';
+slidesBox3.style.transform = 'translate(100%,0)';
+cardsContainer.append(slidesBox3);
+
+const blocks = [slidesBox1, slidesBox2, slidesBox3];
+
+const prev = document.querySelector('#prev');
+const next = document.querySelector('#next');
+
 // START
-let pets = null;
+
+//get pest and form sides
 fetch('../../assets/pets.json')
   .then((response) => response.json())
   .then((petsArr) => {
-    console.log(petsArr);
-
-    pets = petsArr.slice(0, 3);
-    console.log(pets);
-
-    pets.forEach((pet) => {
-      cardsContainer.insertAdjacentHTML(
-        'beforeend',
-        formPet(pet)
-      );
+    prev.addEventListener('click', () => {
+      slidePrev(petsArr);
     });
+    next.addEventListener('click', () => {
+      slideNext(petsArr);
+    });
+
+    set(petsArr);
   });

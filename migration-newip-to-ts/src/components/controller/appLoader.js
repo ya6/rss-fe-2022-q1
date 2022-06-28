397d2da8570904904f39fe4f6037fe154dfa22ba
alex <email@damain.com>
2022-06-28 11:19:14 +0300

    feat: add dev environment: webpack, eslint airbnb-base, task: add initial app, add api key

---
+++ migration-newip-to-ts/src/components/controller/appLoader.js
@@ -0,0 +1,11 @@
+import Loader from './loader';
+
+class AppLoader extends Loader {
+  constructor() {
+    super('https://newsapi.org/v2/', {
+      apiKey: 'cd32042d4b8845fbba3c53ed2d341562', // получите свой ключ https://newsapi.org/
+    });
+  }
+}
+
+export default AppLoader;

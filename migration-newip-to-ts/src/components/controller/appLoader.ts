97b90cf3a65cb4c06f004991535950511d290d67
alex <email@damain.com>
2022-07-03 21:48:26 +0300

    fix: add new api key

--- migration-newip-to-ts/src/components/controller/appLoader.ts
+++ migration-newip-to-ts/src/components/controller/appLoader.ts
@@ -9,7 +9,8 @@ class AppLoader extends Loader<string, OptionsType> {
     // super('', {
     super('https://newsapi.org/v2/', {
     // super('https://nodenews.herokuapp.com/', {
-      apiKey: 'cd32042d4b8845fbba3c53ed2d341562', // get yours key https://newsapi.org/
+      // apiKey: 'cd32042d4b8845fbba3c53ed2d341562', // get yours key https://newsapi.org/ old
+      apiKey: 'eb0f5088f06a4301bab53e7b8e997aa6', // get yours key https://newsapi.org/ new
     });
   }
 }
